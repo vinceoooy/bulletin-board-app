12d96940b89265c9a9d7861b2e0bf8ca301b1c9b
Vince Pag-ong <131712575+vinceoooy@users.noreply.github.com>
2025-11-27 22:53:21 -0700

    Add Terraform configuration for AWS S3 bucket

--- iac/main.tf
+++ iac/main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "vinceoobulletin-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
 
